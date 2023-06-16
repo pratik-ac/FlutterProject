be3f38561a55f221c4626d283e570fdc45f3c783
pratik <pra7ik@outlook.com>
2023-06-16 23:17:17 +0545

    Second commit

--- lib/card_design.dart
+++ lib/card_design.dart
@@ -11,7 +11,7 @@ class CardDesign extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.blue,
+      backgroundColor: Colors.white, // backgroundColor: Colors.blue,
       appBar: AppBar(
         title: Text(Strings.appName),
         backgroundColor: const Color.fromARGB(255, 4, 238, 183),
--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -3,4 +3,5 @@ class AppImages {
   static String memePic = 'assets/images/meme.jpeg';
   static String simpsPic = 'assets/images/simps.jpeg';
   static String hiesenbergPic = 'assets/images/hysenberg.jpeg';
+  static String cardPic = 'assets/images/cards.png';
 }
